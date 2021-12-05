16f3431e7b5cd322863d7bb377987be91c28257f
PuShO4Ek <Stepan.Ulyanov.2007@mail.ru>
2021-12-05 19:35:38 +0400

    33.5.3 finish #1

--- main.cpp
+++ main.cpp
@@ -1,39 +1,70 @@
 #include <iostream>
+#include <vector>
 #include <string>
-#include <cstdlib>
-#include <ctime>
 //#include "boost/asio.hpp"
 
 using namespace std;
 //using namespace boost::asio;
 
-int random(const int& min, const int& max) {
-    return (rand() % (max - min + 1)) + min;
-}
+template<typename T1, typename T2>
+struct MyMapIterator {
+    T1 key;
+    T2 value;
+};
 
-void generate(const int& n) {
-    srand(time(nullptr));
-    int n1 = random(1, 9);
-    int n2 = random(1,9);
-    while (n1 == n2) n2 = random(1, 9);
+template<typename T1, typename T2>
+class MyMap {
+    vector<T1> key;
+    vector<T2> value;
 
-    if (n == n1) throw (string) "you win";
-    if (n == n2) throw (string) "you lose";
-}
+  public:
+
+    void add(T1 n1, T2 n2) {
+        key.push_back(n1);
+        value.push_back(n2);
+    }
+
+    void remove(T1 _key) {
+        for (int i = 0; i < key.size(); i++) {
+            if (key[i] == _key) {
+                for (int j = i; j < key.size()-1; i++) {
+                    key[j] = key[j+1];
+                    value[j] = value[j+1];
+                }
+                key.resize(key.size()-1);
+                value.resize(value.size()-1);
+            }
+        }
+    }
+
+    MyMapIterator<T1, T2> find(T1 _key) {
+        MyMapIterator<T1, T2> s;
+        for (int i = 0; i < key.size(); i++) {
+            if (key[i] == _key) {
+                s.key = key[i];
+                s.value = value[i];
+            }
+        }
+        return s;
+    }
+
+    void print() {
+        for (int i = 0; i < key.size(); i++) {
+            cout << key[i] << " - " << value[i] << endl;
+        }
+    }
+};
 
 int main() {
-    int attempts = 0;
-    do {
-        int n;
-        cout << ">>"; cin >> n;
-        try {
-            attempts+=1;
-            generate(n);
-        }
-        catch (const string& x) {
-            cout << x << endl << "attempts: " << attempts << endl;
-            return 0;
-        }
-        cout << "nothing" << endl;
-    } while (true);
+    MyMap<int, string> s;
+    s.add(1, "hello");
+    s.add(2, "privet");
+
+    s.print();
+
+    auto it = s.find(2);
+    cout << "!" << it.value << endl;
+
+    s.remove(2);
+    s.print();
 }
